21ef8e1e520c782e5426be8e6da1ed50bb584249
silent-sea1119 <sandragonzales56t@gmail.com>
2017-05-19 15:55:10 +0200

    implementing routing and listening on server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+"use strict"
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+// First middleware to define folder for static file
+app.use(express.static('public'));
+
+// routing for homepage
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname, public, 'index.html'));
+})
+
+// runing server for listening port 
+app.listen(3000, function(){
+  console.log("Server started and listening on port 3000");
+})
