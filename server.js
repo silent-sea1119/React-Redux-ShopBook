2e053de27cf52f6462f379eb893183dc393e1bfd
silent-sea1119 <sandragonzales56t@gmail.com>
2017-05-19 17:05:43 +0200

    implementing webpack.config.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static('public'));
 
 // routing for homepage
 app.get('/', function(req, res){
-  res.sendFile(path.resolve(__dirname, public, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-// runing server for listening port 
+// runing server for listening port
 app.listen(3000, function(){
   console.log("Server started and listening on port 3000");
 })
