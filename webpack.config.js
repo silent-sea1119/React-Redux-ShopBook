7e98b6e6192c1f2a7ef295232fd87f26de41af3b
silent-sea1119 <sandragonzales56t@gmail.com>
2017-06-19 07:54:54 +0200

    instaling axios and redux-thunk for conecting the client to the api

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/client.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
